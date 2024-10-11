fcae972546d120556272f57a2d267954b5c527b0
GregoirePachebat <114913256+GregoirePachebat@users.noreply.github.com>
2024-10-11 15:49:10 +0200

    ex05 adavace

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -3,15 +3,32 @@
 
 Least Square
 """
-
+import sys
+sys.path.append("../")
 import numpy as np
+from ex02.template.costs import compute_loss
 
 
 def least_squares(y, tx):
-    """calculate the least squares."""
+    """calculate the least squares.
+    
+    input :
+    y : np.array (N,1)
+    The training output
+    tx : np.array (N, D)
+    The training input expanded or not
+    
+    return :
+    w : np.array
+    the weights of the model
+    mse : float
+    The training error"""
     # ***************************************************
     # INSERT YOUR CODE HERE
     # least squares: TODO
     # returns mse, and optimal weights
+    w = np.linalg.solve(tx.T@tx, tx.T@y)
+    mse = compute_loss(y, tx, w)
+    return w, mse
     # ***************************************************
-    raise NotImplementedError
+
